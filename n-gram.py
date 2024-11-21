54c9cb06bfd96e22c47775543b66648d75caba57
Yukihito Nakaya <>
2024-11-22 01:14:04 +0900

    転置インデックス作成

--- n-gram.py
+++ n-gram.py
@@ -1,12 +1,34 @@
 import os
 import csv
+import pickle
+from collections import defaultdict
+
+#転置インデックス作成
+def create_index(csv_file,index_file):
+    index = defaultdict(list)
+    columns =["郵便番号","都道府県", "市区町村", "町域", "京都通り名", "字丁目", "事業所名", "事業所住所"]
+
+    with open(csv_file, encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for id , row in enumerate(reader):
+            for col in columns:
+                text = row[col]
+                if text:
+                    text = text.replace(" ", "").replace(" ", "")
+                    for i in range(len(text) - 1):
+                        token = text[i:i + 2]
+                        index[token].append(id)
+    # インデックのファイル保存
+    with open(index_file,"wb") as file:
+        pickle.dump(index,file)
+
 def main():
     csv_file = "zenkoku.csv" #csvファイル取得
     index_file = "index.pkl"
 
     # インデックスファイルの作成(初回だけ)
     if not os.path.exists(index_file):
-        #インデックス作成処理
+        create_index(csv_file,index_file)
         print('ok')
     
     # 検索クエリ取得
